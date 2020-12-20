a8943d92ca851ad8ab81b0467b06c7af399a6475
qnrjs42 <qnrjs42@naver.com>
2020-12-20 16:06:45 +0900

    1-4. Recap Refactor

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,14 +4,19 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
   console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if(action.type === "MINUS") {
-    return count - 1;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  return count;
 };
 
 const countStore = createStore(countModifier);
@@ -23,12 +28,12 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-   countStore.dispatch({ type: "ADD" });
+   countStore.dispatch({ type: ADD });
 }
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
