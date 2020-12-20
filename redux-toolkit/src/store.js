daed59087769f0bcf00c72e72d2712d1068a04ed
qnrjs42 <qnrjs42@naver.com>
2020-12-20 21:13:14 +0900

    4-2. createReducer

--- redux-toolkit/src/store.js
+++ redux-toolkit/src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const ADD = "ADD";
 const DELETE = "DELETE";
@@ -7,17 +7,28 @@ const DELETE = "DELETE";
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      console.log(action);
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-}
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       console.log(action);
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// }
+
+// createReducer는
+// state를 mutate하거나
+// 새로운 state를 리턴할 수 있다.
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => 
+    state.filter((toDo) => toDo.id !== action.payload)
+});
 
 const store = createStore(reducer);
 
