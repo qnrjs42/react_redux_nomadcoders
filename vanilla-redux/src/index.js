cdb816933fef70e21dea63b323c559735055d844
qnrjs42 <qnrjs42@naver.com>
2020-12-20 16:34:12 +0900

    2-1. State Mutation

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -12,15 +12,17 @@ const reducer = (state = [], action) => {
   console.log(state, action);
   switch(action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: Date.now() }];
     case DELETE_TODO:
-      return [];
+      return [...state, { text: action.text }];
     default:
       return state;
   }
 }
 
 const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const createToDo = (toDo) => {
   const li = document.createElement("li");
